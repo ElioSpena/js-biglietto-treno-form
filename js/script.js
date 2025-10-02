4cfcfee6ce8d8fb0ff1f5013fb67a89c3b81ec60
ElioSpena <elio.spena.94@gmail.com>
2025-10-02 12:52:57 +0200

    feat: creata funzione ed inserito gli elementi modificati nel DOM

--- js/script.js
+++ js/script.js
@@ -2,17 +2,18 @@
 
 const inputKm = document.getElementById("km-number");
 const inputAge = document.getElementById("age");
-const ticketForm = document.querySelector("form");
 const userNameInput = document.getElementById("user-name");
 const userSurnameInput = document.getElementById("surname");
 const userStationInput = document.getElementById("station");
 const userTelInput = document.getElementById("phone");
 const userEmailInput = document.getElementById("email");
 const userTermsInput = document.getElementById("grid-check");
-console.dir(userTermsInput);
+
+
 
 //recupero gli elementi dal DOM
 
+const ticketForm = document.querySelector("form");
 const passengerNameElem = document.getElementById("passenger");
 const passengerAgeElem = document.getElementById("passenger-age");
 const passengerTelElem = document.getElementById("passenger-tel");
@@ -21,14 +22,20 @@ const passengerStationElem = document.getElementById("passenger-station");
 const passengerKmElem = document.getElementById("passenger-km");
 const passengerDiscountElem = document.getElementById("passenger-discount");
 const ticketPriceElem = document.getElementById("ticket-price");
-console.log(passengerDiscountElem);
+const ticketCard = document.getElementById("ticket-card");
+const intercityElem = document.getElementById("intercity");
+const vagonElem = document.getElementById("vagon");
+
+
 
 
 //al submit stampo in pagina i dati in ingresso 
+
 ticketForm.addEventListener("submit", function (event) {
     event.preventDefault();
 
     //salvo in variabili il valore dell'input utente
+
     const userKm = parseFloat(inputKm.value);
     const userAge = inputAge.value;
     let userName = userNameInput.value.trim();
@@ -38,27 +45,42 @@ ticketForm.addEventListener("submit", function (event) {
     const userEmail = userEmailInput.value.trim();
     const userTerms = userTermsInput.value;
 
+
     //faccio un controllo sull' input utente 
+
     if (userKm <= 0) {
         alert("Errore: Inserire i Km!");
     }
 
     //invoco la funzione salvandola in una variabile
+
     const priceCalc = priceCalculator(userKm, userAge);
     const finalPrice = priceCalc.toFixed(2);
+    const numberIdTrain = getRandomNumb(6);
+    const numberVagon = getRandomNumb(2);
+    console.log(numberIdTrain, numberVagon, finalPrice);
+
 
     //stampo in console il risultato
+
     console.log(`km = ${userKm}; età = ${userAge}; prezzo = ${finalPrice}€`);
 
-    //passo gli elementi al DOM 
+    //passo gli elementi al DOM
+
     passengerNameElem.innerHTML = `${userSurname} ${userName}`;
     passengerAgeElem.innerHTML = userAge;
     passengerTelElem.innerHTML = userTel;
     passengerEmailElem.innerHTML = userEmail;
     passengerKmElem.innerHTML = userKm;
     passengerStationElem.innerHTML = userStation;
+    intercityElem.innerHTML += numberIdTrain;
+    vagonElem.innerHTML += numberVagon;
     //passengerDiscountElem.innerHTML += " " 
-    ticketPriceElem.innerHTML += ` ${finalPrice}`;
+    ticketPriceElem.innerHTML = `Il costo del biglietto è: ${finalPrice} €`;
+
+    //mostro a schermo il ticket
+    ticketCard.classList.remove("d-none");
+    ticketCard.classList.add("d-block");
 
     //ripulisco il form
     ticketForm.reset();
@@ -67,7 +89,13 @@ ticketForm.addEventListener("submit", function (event) {
 
 
 //FUNZIONI
-// creo una funzione per calcolare il prezzo 
+
+/**creo una funzione per calcolare il prezzo
+ * 
+ * @param {number}; km 
+ * @param {string}; età 
+ * @returns {string} prezzo in decimali
+ */
 
 function priceCalculator(km, age) {
 
@@ -89,3 +117,24 @@ function priceCalculator(km, age) {
 
     return price;
 }
+
+/**creo una funzione che genera numeri casuali per quante cifre vogliamo
+ * @param {number}; cifra di numeri da creare
+ * @returns {string}; numero casuale * cifra in stringa 
+ */
+
+function getRandomNumb(cifra) {
+    let randomNumber = "";
+    for (let i = 0; i < cifra; i++) {
+        let curNumber
+        if (i === 0) {
+            curNumber = Math.floor(Math.random() * 9) + 1;
+        } else {
+            curNumber = Math.floor(Math.random() * 10);
+        }
+        randomNumber += curNumber;
+    }
+
+    return randomNumber
+}
+
